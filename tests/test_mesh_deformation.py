6bcdce32febb1e83d81b4a8ed17b87ac9e279a53
martinlanton <lantonmartin@gmail.com>
2022-07-24 00:43:18 -0700

    Added test for reverting to base with 100% value

--- tests/test_mesh_deformation.py
+++ tests/test_mesh_deformation.py
@@ -8,7 +8,30 @@ from sym_mesh import mesh_modification
 
 
 class TestMeshDeformation(common.BaseTest):
+    def test_revert_to_current(self):
+        """Test that reverting to base with a value of 100% doesn't revert anything."""
+        geo_table = table.GeometryTable(self.asym_cube)
+        sym_table = table.GeometryTable(self.sym_cube)
+        mesh_modifier = mesh_modification.MeshModifier()
+        mesh_modifier.revert_to_base(
+            base_table=sym_table, current_table=geo_table, percentage=100
+        )
+
+        vtx_number = len(mc.ls("{}.vtx[*]".format(self.sym_cube), flatten=True))
+
+        expected = [
+            mc.pointPosition("{}.vtx[{}]".format(self.asym_cube, vtx), world=True)
+            for vtx in range(vtx_number)
+        ]
+        result = [
+            mc.pointPosition("{}.vtx[{}]".format(self.asym_cube, vtx), world=True)
+            for vtx in range(vtx_number)
+        ]
+
+        self.assertEqual(expected, result)
+
     def test_revert_to_base(self):
+        """Test that reverting to base with a value of 0% reverts to base."""
         geo_table = table.GeometryTable(self.asym_cube)
         sym_table = table.GeometryTable(self.sym_cube)
         mesh_modifier = mesh_modification.MeshModifier()
@@ -22,11 +45,10 @@ class TestMeshDeformation(common.BaseTest):
             mc.pointPosition("{}.vtx[{}]".format(self.sym_cube, vtx), world=True)
             for vtx in range(vtx_number)
         ]
-        pprint(expected)
         result = [
             mc.pointPosition("{}.vtx[{}]".format(self.asym_cube, vtx), world=True)
             for vtx in range(vtx_number)
         ]
-        pprint(result)
 
         self.assertEqual(expected, result)
+
