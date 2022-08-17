ce555cdd0e7731c2fbc7842ccf3302e29df6d3a4
martinlanton <lantonmartin@gmail.com>
2022-08-16 21:37:06 -0700

    Started working on flipping

--- tests/test_mesh_deformation.py
+++ tests/test_mesh_deformation.py
@@ -210,3 +210,26 @@ class TestBakeDeltas(common.BaseTest):
         self.assertEqual(expected, result)
 
 
+# class TestFlip(common.BaseTest):
+#     def test_symmetrization_x_positive(self):
+#         """Test that reverting to base with a value of 0% reverts to base."""
+#         geo_table = table.GeometryTable(self.asym_cube)
+#         sym_table = table.GeometryTable(self.sym_cube, axis="x", direction="positive")
+#         mesh_modifier = mesh_modification.MeshModifier()
+#         mesh_modifier.flip(base_table=sym_table, target_table=geo_table)
+#
+#         vtx_number = len(mc.ls("{}.vtx[*]".format(self.sym_cube), flatten=True))
+#
+#         expected = [
+#         ]
+#         result = [
+#             mc.pointPosition("{}.vtx[{}]".format(self.asym_cube, vtx), world=True)
+#             for vtx in range(vtx_number)
+#         ]
+#
+#         log.info("Symmetry table : %s", sym_table.symmetry_table)
+#         log.info("Expected : %s", expected)
+#         log.info("Result : %s", result)
+#         self.assertEqual(expected, result)
+
+
