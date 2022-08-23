bb6d5914499db4c13b3538c55f8f6bd759b076b4
martinlanton <lantonmartin@gmail.com>
2022-08-22 20:06:35 -0700

    Started working on controller

--- src/sym_mesh/domain/mesh_modification.py
+++ src/sym_mesh/domain/mesh_modification.py
@@ -3,8 +3,6 @@ import logging
 
 from sym_mesh.domain import commands
 from sym_mesh.domain.dag_path import create_MDagPath
-from sym_mesh.domain.selection import get_sel_vtces_idcs
-
 
 log = logging.getLogger(__name__)
 log.setLevel(logging.INFO)
@@ -13,30 +11,6 @@ log.setLevel(logging.INFO)
 class MeshModifier(object):
     def __init__(self):
         # Attributes
-        self.base = ""
-        self.target = ""
-        self.get_vtcs_selection(reset=True)
-
-        self.base_table = {"objs_path": om2.MDagPath(), "points_pos": om2.MPointArray()}
-        self.temp_base_table = {
-            "objs_path": om2.MDagPath(),
-            "points_pos": om2.MPointArray(),
-        }
-        self.target_table = {
-            "objs_path": om2.MDagPath(),
-            "points_pos": om2.MPointArray(),
-        }
-        self.current_table = {
-            "objs_path": om2.MDagPath(),
-            "points_pos": om2.MPointArray(),
-        }
-        self.sel_vtces_idcs = {"objs_path": om2.MDagPath(), "indices": om2.MIntArray()}
-        self.vtcs_selection = {"objs_path": om2.MDagPath(), "indices": om2.MIntArray()}
-
-        self.symmetry_table = dict()
-        self.non_mirrored_vtcs = list()
-
-        self.are_vertices_stored = False
         self._revert_value = 100
         self.space = om2.MSpace.kObject
         # UNDO LIST
@@ -50,25 +24,6 @@ class MeshModifier(object):
     @revert_value.setter
     def revert_value(self, value):
         self._revert_value = value
-
-    def get_vtcs_selection(self, reset=False):
-        """Get the current selection of vertices and set it.
-        :param reset: Set whether the currently stored selection should be set
-        to an empty selection.
-        :type reset: bool
-        """
-        if reset:
-            self.vtcs_selection = {
-                "objs_path": om2.MDagPath(),
-                "indices": om2.MIntArray(),
-            }
-            self.are_vertices_stored = False
-        else:
-            self.vtcs_selection = get_sel_vtces_idcs()
-            if len(self.vtcs_selection["indices"]) > 0:
-                self.are_vertices_stored = True
-            else:
-                self.are_vertices_stored = False
 
     def undo(self):
         """Undo the last move stored in the undo queue."""
@@ -91,195 +46,6 @@ class MeshModifier(object):
 
         last_action.redo()
         self.undo_queue.append(last_action)
-
-    # def revert_selected_to_base(self, revert_value=None):
-    #     """
-    #     Revert selected mesh or vertices to base from the registered target
-    #     value, using vertices selection (if one has been stored or is active) or
-    #     on the whole mesh.
-    #
-    #     """
-    #     # Get selected vertices indices
-    #     self.sel_vtces_idcs = self.get_sel_vtces_idcs()
-    #     # If no vertices are currently selected
-    #     if self.sel_vtces_idcs["indices"].__len__() == 0:
-    #         # If a selection is stored
-    #         if self.vtcs_selection["indices"].__len__() > 0:
-    #             # Replace indices using stored selection
-    #             self.sel_vtces_idcs["indices"] = self.vtcs_selection["indices"]
-    #
-    #     # Update revert value
-    #     if revert_value is not None:
-    #         self._revert_value = revert_value
-    #
-    #     for dag_path in self.sel_vtces_idcs["objs_path"]:
-    #         # Update current mesh table
-    #         self.current_table = self.get_selected_mesh_points(dag_path)
-    #
-    #         log.debug(self.current_table["objs_path"].fullPathName())
-    #
-    #         self.undo_table = {
-    #             "objs_path": self.current_table["objs_path"].fullPathName(),
-    #             "points_pos": self.current_table["points_pos"],
-    #         }
-    #
-    #         self.undo.append(self.undo_table)
-    #
-    #         # Check if base is registered
-    #         if not self.base_table["points_pos"]:
-    #             log.error("No base selected")
-    #             return
-    #         # Check if target is registered
-    #         elif not self.target_table["points_pos"]:
-    #             log.info("No target registered")
-    #             return
-    #         # Check if something is selected
-    #         elif not self.sel_vtces_idcs["objs_path"]:
-    #             log.info("Nothing is selected")
-    #             return
-    #         # Revert to base
-    #         else:
-    #             self.revert_to_base(
-    #                 self.base_table["points_pos"],
-    #                 self.target_table["points_pos"],
-    #                 self.sel_vtces_idcs["indices"],
-    #                 self._revert_value,
-    #                 dag_path,
-    #                 self.space,
-    #             )
-
-    # def revert_selected_to_base_live(self, revert_value=None):
-    #     """
-    #     Revert selected mesh or vertices to base from the current value, using
-    #     vertices selection (if one has been stored or is active) or on the whole
-    #     mesh.
-    #
-    #     """
-    #     # Get selected vertices indices
-    #     self.sel_vtces_idcs = self.get_sel_vtces_idcs()
-    #     # If no vertices are currently selected
-    #     if self.sel_vtces_idcs["indices"].__len__() == 0:
-    #         # If a selection is stored
-    #         if self.vtcs_selection["indices"].__len__() > 0:
-    #             # Replace indices using stored selection
-    #             self.sel_vtces_idcs["indices"] = self.vtcs_selection["indices"]
-    #
-    #     # Update revert value
-    #     if revert_value is not None:
-    #         self._revert_value = revert_value
-    #
-    #     for dag_path in self.sel_vtces_idcs["objs_path"]:
-    #         # Update current mesh table
-    #         self.current_table = self.get_selected_mesh_points(dag_path)
-    #         self.temp_base_table = self.get_selected_mesh_points(
-    #             self.base_table["objs_path"]
-    #         )
-    #
-    #         self.undo_table = {
-    #             "objs_path": self.current_table["objs_path"].fullPathName(),
-    #             "points_pos": self.current_table["points_pos"],
-    #         }
-    #
-    #         self.undo.append(self.undo_table)
-    #
-    #         # Check if base is registered
-    #         if not self.temp_base_table["points_pos"]:
-    #             log.info("No base selected")
-    #             return
-    #         # Check if something is selected
-    #         elif not self.sel_vtces_idcs["objs_path"]:
-    #             log.info("Nothing is selected")
-    #             return
-    #         # Revert to base
-    #         else:
-    #             self.revert_to_base(
-    #                 self.temp_base_table["points_pos"],
-    #                 self.current_table["points_pos"],
-    #                 self.sel_vtces_idcs["indices"],
-    #                 self._revert_value,
-    #                 dag_path,
-    #                 self.space,
-    #             )
-
-    # def bake_difference_on_selected(self):
-    #     """
-    #     Bake the difference between base and target on the selected meshes using
-    #     vertices selection (if one has been stored or is active) or on the whole
-    #     mesh.
-    #
-    #     """
-    #     # Get selected vertices indices
-    #     self.sel_vtces_idcs = self.get_sel_vtces_idcs()
-    #     # If no vertices are currently selected
-    #     if self.sel_vtces_idcs["indices"].__len__() == 0:
-    #         # If a selection is stored
-    #         if self.vtcs_selection["indices"].__len__() > 0:
-    #             # Replace indices using stored selection
-    #             self.sel_vtces_idcs["indices"] = self.vtcs_selection["indices"]
-    #
-    #     for dag_path in self.sel_vtces_idcs["objs_path"]:
-    #         # Update current mesh table
-    #         self.current_table = self.get_selected_mesh_points(dag_path)
-    #
-    #         log.debug(self.current_table["objs_path"].fullPathName())
-    #
-    #         self.undo_table = {
-    #             "objs_path": self.current_table["objs_path"].fullPathName(),
-    #             "points_pos": self.current_table["points_pos"],
-    #         }
-    #
-    #         self.undo.append(self.undo_table)
-    #
-    #         # Check if base is registered
-    #         if not self.base_table["points_pos"]:
-    #             log.info("No base selected")
-    #             return
-    #         # Check if target is registered
-    #         elif not self.target_table["points_pos"]:
-    #             log.info("No target registered")
-    #             return
-    #         # Check if something is selected
-    #         elif not self.sel_vtces_idcs["objs_path"]:
-    #             log.info("Nothing is selected")
-    #             return
-    #         # Revert to base
-    #         else:
-    #             self.bake_difference(
-    #                 self.base_table["points_pos"],
-    #                 self.target_table["points_pos"],
-    #                 self.current_table["points_pos"],
-    #                 self.sel_vtces_idcs["indices"],
-    #                 dag_path,
-    #                 self.space,
-    #             )
-    #
-    # def select_stored_vertices(self):
-    #     if len(self.vtcs_selection["indices"]) == 0:
-    #         log.warning("No vertex selection stored")
-    #     else:
-    #         vtcs_to_select = om2.MSelectionList()
-    #         MItVtx = om2.MItMeshVertex(self.vtcs_selection["objs_path"][0])
-    #         while not MItVtx.isDone():
-    #             if MItVtx.index() in self.vtcs_selection["indices"]:
-    #                 vtcs_to_select.add(
-    #                     (self.vtcs_selection["objs_path"][0], MItVtx.currentItem())
-    #                 )
-    #             MItVtx.next()
-    #         om2.MGlobal.setActiveSelectionList(vtcs_to_select)
-    #
-    # def select_non_mirrored_vertices(self):
-    #     if len(self.non_mirrored_vtcs) == 0:
-    #         log.info("Model is symmetrical, no vertices to select")
-    #     else:
-    #         vtcs_to_select = om2.MSelectionList()
-    #         MItVtx = om2.MItMeshVertex(self.base_table["objs_path"])
-    #         while not MItVtx.isDone():
-    #             if MItVtx.index() in self.non_mirrored_vtcs:
-    #                 vtcs_to_select.add(
-    #                     (self.base_table["objs_path"], MItVtx.currentItem())
-    #                 )
-    #             MItVtx.next()
-    #         om2.MGlobal.setActiveSelectionList(vtcs_to_select)
 
     def bake_difference(
         self,
