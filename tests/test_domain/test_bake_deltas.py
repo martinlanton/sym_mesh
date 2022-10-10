1ac7811b9fc87c49f1d29120ff190ef17387cbdc
martinlanton <lantonmartin@gmail.com>
2022-10-09 22:49:03 -0700

    Added tests for vertex selection for delta baking

--- tests/test_domain/test_bake_deltas.py
+++ tests/test_domain/test_bake_deltas.py
@@ -46,8 +46,8 @@ class TestBakeDeltas(common.BaseTest):
         self.assertEqual(self.expected_sym_position, result)
 
     def test_bake_deltas_with_vertex_selection(self):
-        """Test that reverting the mesh to base when a proper vertex selection is
-        provided only reverts the selected vertices."""
+        """Test that baking the deltas on a mesh when a proper vertex selection is
+        provided only bakes the deltas for the selected vertices."""
         sym_table = table.GeometryTable(self.sym_cube)
         geo_table = table.GeometryTable(self.asym_cube)
         mc.select("{}.vtx[1]".format(self.asym_cube))
@@ -80,5 +80,43 @@ class TestBakeDeltas(common.BaseTest):
         log.info("Result : %s", result)
         self.assertEqual(expected_sym_position, result)
 
+    def test_bake_delta_with_no_vertex_selection(self):
+        """Test that baking the deltas on a mesh when a proper vertex selection is
+        provided only bakes the deltas for the selected vertices."""
+        geo_table = table.GeometryTable(self.asym_cube)
+        sym_table = table.GeometryTable(self.sym_cube)
+        mc.select(clear=True)
+        vertex_selection = selection.VertexSelection()
+        mesh_modifier = mesh_modification.MeshModifier()
+        mesh_modifier.bake_difference(
+            base_table=sym_table,
+            target_table=geo_table,
+            vertex_selection=vertex_selection,
+            target_dag_path=self.other_cube,
+        )
 
-# TODO : add tests with active vertices selection
+        result = [
+            mc.pointPosition("{}.vtx[{}]".format(self.other_cube, vtx), world=True)
+            for vtx in range(self.vtx_number)
+        ]
+        self.assertEqual(self.expected_asym_position, result)
+
+    def test_bake_delta_with_geometry_selected_for_vertex_selection(self):
+        """Test that baking delta functions properly on one geometry."""
+        geo_table = table.GeometryTable(self.asym_cube)
+        sym_table = table.GeometryTable(self.sym_cube)
+        mc.select(self.asym_cube)
+        vertex_selection = selection.VertexSelection()
+        mesh_modifier = mesh_modification.MeshModifier()
+        mesh_modifier.bake_difference(
+            base_table=sym_table,
+            target_table=geo_table,
+            vertex_selection=vertex_selection,
+            target_dag_path=self.other_cube,
+        )
+
+        result = [
+            mc.pointPosition("{}.vtx[{}]".format(self.other_cube, vtx), world=True)
+            for vtx in range(self.vtx_number)
+        ]
+        self.assertEqual(self.expected_asym_position, result)
