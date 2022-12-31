489d4e2c07f338f8446245efd8ea14a73827ef33
martinlanton <lantonmartin@gmail.com>
2022-12-31 14:34:18 -0800

    Adding test for multiple selection for VertexSeletion

--- tests/test_domain/test_selection.py
+++ tests/test_domain/test_selection.py
@@ -8,9 +8,6 @@ log = logging.getLogger(__name__)
 
 
 class TestVertexSelection(common.BaseTest):
-    # TODO : add error test when creating a VertexSelection with an active
-    #  selection containing more than one mesh.
-
     def test_selecting_empty_stored_vertices(self):
         mc.select(clear=True)
 
@@ -28,6 +25,24 @@ class TestVertexSelection(common.BaseTest):
         to_select = []
         for vtx in [1, 3, 5, 7]:
             to_select.append("{}.vtx[{}]".format(self.test_extract_axes_cube, vtx))
+        mc.select(to_select)
+
+        vtx_selection = selection.VertexSelection()
+        mc.select(clear=True)
+
+        vtx_selection.select()
+
+        result = mc.ls(sl=True)
+        expected = to_select
+
+        self.assertEqual(expected, result)
+
+    def test_selecting_stored_vertices_for_multiple_selection(self):
+        to_select = []
+        to_not_select = []
+        for vtx in [1, 3, 5, 7]:
+            to_select.append("{}.vtx[{}]".format(self.test_extract_axes_cube, vtx))
+            to_not_select.append("{}.vtx[{}]".format(self.sym_cube, vtx))
         mc.select(to_select)
 
         vtx_selection = selection.VertexSelection()
