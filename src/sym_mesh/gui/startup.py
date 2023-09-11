17d88212985f008f793e6f72e43ad0ccbe73e2c8
martinlanton <lantonmartin@gmail.com>
2023-09-11 13:09:31 -0700

    passed dockable as a parameter at gui startup

--- src/sym_mesh/gui/startup.py
+++ src/sym_mesh/gui/startup.py
@@ -2,10 +2,10 @@ from sym_mesh.gui import connection_widget
 from sym_mesh.gui import dockable_dialog
 
 
-def startup():  # pragma: no cover
+def startup(dockable=True):  # pragma: no cover
     dialog = dockable_dialog.DockableDialog.instance(
         connection_widget.ConnectionWidget
     )  # pragma: no cover
-    dialog.show(dockable=True)  # pragma: no cover
+    dialog.show(dockable=dockable)  # pragma: no cover
 
     return dialog
